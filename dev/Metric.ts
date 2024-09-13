8195e31d7e8985485adf07c09de09140fe4a6743
Lawrence Folsom Smith <smit4407@eceprog5.ecn.purdue.edu>
2024-09-13 00:33:44 -0400

    metric added

---
+++ dev/Metric.ts
@@ -0,0 +1,15 @@
+class Metric {
+    private score: number;
+
+    constructor(score: number = 0) {
+        this.score = score;
+    }
+
+    getScore(): boolean {
+        return this.score === 0;
+    }
+
+    updateScore(newScore: number): void {
+        this.score = newScore;
+    }
+}
